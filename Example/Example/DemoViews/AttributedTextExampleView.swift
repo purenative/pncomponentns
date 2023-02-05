286284972597c8b88a6f27c97c2fc93fb9f43d78
Artem Eremeev <jeremy1002@yandex.ru>
2023-02-05 20:33:41 +0300

    feat(example): add NSMutableAttributedString extensions demo

--- Example/Example/DemoViews/AttributedTextExampleView.swift
+++ Example/Example/DemoViews/AttributedTextExampleView.swift
@@ -4,16 +4,34 @@ import PNComponents
 struct AttributedTextExampleView: View {
     
     @State
-    var attributedString = NSAttributedString(string: "Hello!\nThis is a test text", attributes: [
-        .font: UIFont.systemFont(ofSize: 20, weight: .bold),
-        .foregroundColor: UIColor.systemOrange
-    ])
+    var attributedString = Self.createAttributedString()
     
     var body: some View {
         ZStack {
             AttributedText(attributedString: $attributedString)
                 .padding(24)
         }
+    }
+    
+    private static func createAttributedString() -> NSAttributedString {
+        NSMutableAttributedString()
+            .appending(string: "Hello", with: [
+                .font: UIFont.systemFont(ofSize: 30, weight: .bold),
+                .foregroundColor: UIColor.systemRed
+            ])
+            .appending(string: "\n\n")
+            .appending(string: "This is a test text", with: [
+                .font: UIFont.systemFont(ofSize: 20),
+                .foregroundColor: UIColor.systemOrange
+            ])
+            .appending(string: "\n")
+            .appending(string: "Just for example", with: [
+                .foregroundColor: UIColor.systemYellow
+            ])
+            .adding(attributes: [
+                .font: UIFont.systemFont(ofSize: 30, weight: .bold),
+                .foregroundColor: UIColor.systemBlue
+            ], for: "test")
     }
     
 }
