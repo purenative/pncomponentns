9c47e3fac7629e8c3351ae911a54dc1148e43cf1
JeremySnor <jeremy1002@yandex.ru>
2023-08-08 16:13:32 +0300

    Feature/segment control selector layer (#15)
    * feat(package): add segment control selector layer builder
    
    * feat(example): update segment control demo

--- Sources/UI/Controls/SegmentControl/SegmentControl.swift
+++ Sources/UI/Controls/SegmentControl/SegmentControl.swift
@@ -1,6 +1,6 @@
 import SwiftUI
 
-public struct SegmentControl<Item: SegmentControlItem, ItemView: View>: View {
+public struct SegmentControl<Item: SegmentControlItem, ItemView: View, SelectorView: View>: View {
     
     let spacing: CGFloat
     
@@ -8,18 +8,34 @@ public struct SegmentControl<Item: SegmentControlItem, ItemView: View>: View {
     @Binding
     var selectedItem: Item
     let itemViewBuilder: (Item, Bool) -> ItemView
+    let selectorViewBuilder: () -> SelectorView
     
-    public init(spacing: CGFloat = .zero, items: [Item], selectedItem: Binding<Item>, @ViewBuilder itemViewBuilder: @escaping (Item, Bool) -> ItemView) {
+    public init(spacing: CGFloat = .zero, items: [Item], selectedItem: Binding<Item>, @ViewBuilder itemViewBuilder: @escaping (Item, Bool) -> ItemView, @ViewBuilder selectorViewBuilder: @escaping () -> SelectorView) {
         self.spacing = spacing
         self.items = items
         self._selectedItem = selectedItem
         self.itemViewBuilder = itemViewBuilder
+        self.selectorViewBuilder = selectorViewBuilder
+    }
+    
+    public init(spacing: CGFloat = .zero, items: [Item], selectedItem: Binding<Item>, @ViewBuilder itemViewBuilder: @escaping (Item, Bool) -> ItemView) where SelectorView == EmptyView {
+        self.spacing = spacing
+        self.items = items
+        self._selectedItem = selectedItem
+        self.itemViewBuilder = itemViewBuilder
+        self.selectorViewBuilder = { EmptyView() }
     }
     
     public var body: some View {
         VStack {
             GeometryReader { geometry in
                 let itemWidth = (geometry.size.width - CGFloat(items.count - 1) * spacing) / CGFloat(items.count)
+                let selectedItemIndex = items.firstIndex(where: { $0.id == selectedItem.id }) ?? 0
+                let selectorXOffset = CGFloat(selectedItemIndex) * (itemWidth + spacing)
+                
+                selectorViewBuilder()
+                    .frame(width: itemWidth)
+                    .offset(x: selectorXOffset)
                 
                 HStack(alignment: .center, spacing: spacing) {
                     ForEach(items) { item in
